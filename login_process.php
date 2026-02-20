1fce6631492a2e102e1e3536a0d63c6723ee481d
Valerie Putri <valerie.putritivanes@gmail.com>
2026-02-20 20:32:23 +0700

    Create login_process.php

---
+++ login_process.php
@@ -0,0 +1,9 @@
+<?php
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if ($username == 'admin' && 
+        $password == '123456') {
+    echo 'Login succeeds.';
+    } else echo 'Login failed.';
+?>
